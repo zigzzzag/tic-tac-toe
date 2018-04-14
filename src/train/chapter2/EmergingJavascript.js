e9697b4bccc997deedc80ac40714146bcf759c04
zigzzzag <maximnikiforov1991@gmail.com>
2018-04-14 22:15:48 +0700

    add promises

--- src/train/chapter2/EmergingJavascript.js
+++ src/train/chapter2/EmergingJavascript.js
@@ -100,4 +100,24 @@ let pFullName = {
 };
 console.log(pFullName);
 
+console.log('-------------------------------------------PROMISES-----------------------------------------------------');
+
+//todo fix this
+const getFakeMembers = count => new Promise((resolves, rejects) => {
+    const api = `https://api.randomuser.me/?nat=US&results=${count}`;
+    const request = new XMLHttpRequest();
+    request.open('GET', api);
+    request.onload = () =>
+        (request.status === 200) ?
+            resolves(JSON.parse(request.response).results) :
+            reject(Error(request.statusText));
+    request.onerror = (err) => rejects(err);
+    request.send()
+});
+getFakeMembers(5).then(
+    members => console.log(members),
+    err => console.error(
+        new Error("cannot load members from randomuser.me"))
+);
+
 console.log('--------------------------------------------------------------------------------------------------------');
