dfb6bc188464bb0d6f058a6ac3b68f45d3088c0f
zigzzzag <maximnikiforov1991@gmail.com>
2018-04-15 18:07:53 +0700

    add modules

---
+++ src/train/chapter2/module-test.js
@@ -0,0 +1,6 @@
+console.log('-----------------------------------------ES-6-MODULES---------------------------------------------------');
+
+import {print, log} from './text-helpers'
+
+print('printing a message');
+log('logging a message');
