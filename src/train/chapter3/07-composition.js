2d30e154b2f779a53e1615ab16cb0f20281f10d2
zigzzzag <maximnikiforov1991@gmail.com>
2018-04-21 17:37:30 +0700

    add composition

---
+++ src/train/chapter3/07-composition.js
@@ -0,0 +1,23 @@
+const compose = (...fns) =>
+    (arg) =>
+        fns.reduce(
+            (composed, f) => f(composed),
+            arg
+        );
+
+const civilianHours = (template) =>
+    template.replace("hh", "11")
+        .replace("mm", "22")
+        .replace("ss", "33");
+
+const appendAMPM = (template) =>
+    template.replace("tt", "PM");
+
+const both = compose(
+    civilianHours,
+    appendAMPM
+);
+
+const template = "hh:mm:ss tt";
+
+console.log(both(template));
